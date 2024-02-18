c723ea8daa5568fbde5c56db6dd4eb2cb1ac417e
Muhammad Junaid <648mjunaid@gmail.com>
2024-02-18 10:40:51 +0500

    type sage components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import products from "@/assets/data/products";
-import ProductListItem from "@/src/components/ProductListItem";
+import { View } from "@/components/Themed";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListItem";
 
 export default function TabOneScreen() {
   return (
-    <>
+    <View>
       <ProductListItem product={products[0]} />
       <ProductListItem product={products[1]} />
-    </>
+    </View>
   );
 }
